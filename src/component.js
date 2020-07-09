c34e115f00d759e3871f3380d195e752f02019b6
Maxim Byts <maxim.90svet@gmail.com>
2020-07-09 20:24:52 +0300

    Add checkbox block

--- src/component.js
+++ src/component.js
@@ -1,6 +1,7 @@
-import './css/style.scss'
+import './project/common.block/style.scss'
 import './project/common.block/logo/logo.scss'
 import './project/common.block/logo/logo_color-change.js'
 import './project/common.block/input-text/input-text.scss'
 import './project/common.block/dropdown/dropdown.scss'
 import './project/common.block/dropdown/dropdown.js'
+import './project/common.block/checkbox/checkbox-custom.scss'
