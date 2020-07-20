d28d44d7a1d7f108671863822ab7d1b09c7b9228
Maxim Byts <maxim.90svet@gmail.com>
2020-07-20 21:24:11 +0300

    Improved paginator, add some functionality

--- src/project/pagination/pagination.js
+++ src/project/pagination/pagination.js
@@ -2,20 +2,15 @@ function setTableCaption(currentPage) {};
 
 document.addEventListener("DOMContentLoaded", function () {
   const paginationForm = document.body.querySelector(".pagination");
-  const forwardButton = paginationForm.querySelector(".pagination__button-forward");
-  const backwardButton = paginationForm.querySelector(".pagination__button-backward");  
-  const items = [paginationForm.children[0].children[0].children[1], 
+  const forwardButton = paginationForm.querySelector(".pagination__button-forward"); 
+  const items = [paginationForm.children[0].children[0].children[0], 
+  paginationForm.children[0].children[0].children[1],
   paginationForm.children[0].children[0].children[2],
   paginationForm.children[0].children[0].children[3],
-  paginationForm.children[0].children[0].children[4],
-  paginationForm.children[0].children[0].children[5]];
+  paginationForm.children[0].children[0].children[4]];
   let currentPage = paginationForm.querySelector(".pagination__current"); 
   
   forwardButton.addEventListener("mouseup", function () {
-    if (parseInt(currentPage.innerText) === 1) {
-      backwardButton.classList.remove("pagination__button_hidden");
-    }
-
     if (items[3].isSameNode(currentPage)) {
       currentPage.classList.remove("pagination__current");
       items[4].classList.add("pagination__current");
@@ -59,4 +54,103 @@ document.addEventListener("DOMContentLoaded", function () {
       currentPage = items[1];
     }
   }); 
+
+  items[0].addEventListener("mouseup", function () {
+    if (!this.classList.contains("pagination__current")) {
+      switch (parseInt(this.innerText)) {
+        case 1:
+          currentPage.classList.remove("pagination__current");
+          this.classList.add("pagination__current");
+          currentPage = this;
+          break;
+        case 2:
+          items[0].innerText = 1;
+          items[1].innerText = 2;
+          items[2].innerText = 3;
+          currentPage.classList.remove("pagination__current");
+          items[1].classList.add("pagination__current");
+          currentPage = items[1];
+          break;
+        case 11:
+          items[0].innerText = 9;
+          items[1].innerText = 10;
+          items[2].innerText = 11;
+          items[3].innerText = "...";
+          currentPage.classList.remove("pagination__current");
+          items[2].classList.add("pagination__current");
+          currentPage = items[2];
+          forwardButton.classList.remove("pagination__button_fade");
+          break;
+        default: 
+          items[2].innerText = items[0].innerText;
+          items[1].innerText = parseInt(items[0].innerText)-1;
+          items[0].innerText = parseInt(items[1].innerText)-1;
+          currentPage.classList.remove("pagination__current");
+          items[2].classList.add("pagination__current");
+          currentPage = items[2];
+          break;
+      }
+    }
+  });
+
+  items[1].addEventListener("mouseup", function () {
+    if (!this.classList.contains("pagination__current")) {
+      currentPage.classList.remove("pagination__current");
+      this.classList.add("pagination__current");
+      currentPage = this;
+    }
+  });
+
+  items[2].addEventListener("mouseup", function () {
+    if (!this.classList.contains("pagination__current")) {
+      switch (parseInt(this.innerText)) {
+        case 13: 
+          currentPage.classList.remove("pagination__current");
+          items[2].classList.add("pagination__current");
+          currentPage = items[2];
+        break;
+        case 12:
+          items[0].innerText = items[1].innerText;
+          items[1].innerText = items[2].innerText;
+          items[2].innerText = parseInt(items[2].innerText) + 1;
+          items[3].innerText = parseInt(items[2].innerText) + 1;
+          currentPage.classList.remove("pagination__current");
+          items[1].classList.add("pagination__current");
+          currentPage = items[1];
+        break;
+        default:
+          items[0].innerText = items[1].innerText;
+          items[1].innerText = items[2].innerText;
+          items[2].innerText = parseInt(items[2].innerText) + 1;
+          currentPage.classList.remove("pagination__current");
+          items[1].classList.add("pagination__current");
+          currentPage = items[1];
+        break;
+      }
+    }
+  });
+
+  items[3].addEventListener("mouseup", function () {
+    if (!this.classList.contains("pagination__current")) {
+      if (parseInt(this.innerText) === 14) {
+        currentPage.classList.remove("pagination__current");
+        items[3].classList.add("pagination__current");
+        currentPage = items[3];
+      }
+    }
+  });
+
+  items[4].addEventListener("mouseup", function () {
+    if (!this.classList.contains("pagination__current")) {
+      items[0].innerText = 11;
+      items[1].innerText = 12;
+      items[2].innerText = 13;
+      items[3].innerText = 14;
+      currentPage.classList.remove("pagination__current");
+      items[4].classList.add("pagination__current");
+      currentPage = items[4];
+      forwardButton.classList.add("pagination__button_fade");
+    }
+  });
+
 });
