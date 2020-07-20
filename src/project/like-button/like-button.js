da8440a7b203e1da24663d4035fbaafb4f0bab15
Maxim Byts <maxim.90svet@gmail.com>
2020-07-20 07:49:26 +0300

    Create range slider (only on webkit)

--- src/project/like-button/like-button.js
+++ src/project/like-button/like-button.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const likeButtonsList = document.body.querySelectorAll(".like-button");
 
   for (let i = 0; i < likeButtonsList.length; i++) {
-    likeButtonsList[i].addEventListener("mousedown", function () {
+    likeButtonsList[i].addEventListener("change", function () {
       const target = this.children[0];
       let input = target.nextElementSibling;
       if (target.checked) {
-        input.innerText = parseInt(input.innerText, 10) - 1;
-      } else {
         input.innerText = parseInt(input.innerText, 10) + 1;
+      } else {
+        input.innerText = parseInt(input.innerText, 10) - 1;
       }
     });
   }
