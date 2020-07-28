1b0442d218f7793304e4b8574640e8b75f618d47
Maxim Byts <maxim.90svet@gmail.com>
2020-07-28 04:37:45 +0300

    Add responsive to footer

--- src/component.js
+++ src/component.js
@@ -49,4 +49,3 @@ import './project/logo/logo_color-change.js' // модификатор окра�
 // Хедер и футер
   import './project/header/header.scss'
   import './project/footer/footer.scss'
-  // import './project/footer-creative/footer-creative.scss'
