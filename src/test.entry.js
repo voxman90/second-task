3b6ee095375d64c839bf7485edbee891757a9ced
Maxim Byts <maxim.90svet@gmail.com>
2021-02-27 23:12:50 +0300

    Improved label, text-field, input-text

--- src/test.entry.js
+++ src/test.entry.js
@@ -1,5 +1,4 @@
 import "./style.scss";
-import "./common.blocks/rate-button/rate-button.scss";
-import "./common.blocks/rate-button/rate-button.js";
-import "./common.blocks/icon/icon.scss";
+import "./common.blocks/input-text/input-text.scss";
+import "./common.blocks/label/label.scss";
 import "./common.blocks/text-field/text-field.scss";
