538769916a12c7cc5f8597b091a4d1e28e7ca55c
Maxim Byts <maxim.90svet@gmail.com>
2021-02-28 00:09:32 +0300

    Improved subcription-field

--- src/test.entry.js
+++ src/test.entry.js
@@ -1,4 +1,6 @@
 import "./style.scss";
+import "./common.blocks/subscription-field/subscription-field.scss";
+import "./common.blocks/icon/icon.scss";
 import "./common.blocks/input-text/input-text.scss";
 import "./common.blocks/label/label.scss";
 import "./common.blocks/text-field/text-field.scss";
