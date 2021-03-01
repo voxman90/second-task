35ebac92f09cfc502e6086cb6ea2bcd01bc6b98f
Maxim Byts <maxim.90svet@gmail.com>
2021-03-01 20:55:38 +0300

    minor changes (bullet-list)

--- src/test.entry.js
+++ src/test.entry.js
@@ -1,4 +1,5 @@
 import "./style.scss";
+import "./common.blocks/bullet-list/bullet-list.scss";
 import "./common.blocks/input-text/input-text.scss";
 import "./common.blocks/subscription-field/subscription-field.scss";
 import "./common.blocks/rate-button/rate-button.scss";
