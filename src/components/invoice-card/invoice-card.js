45289ac786e661b746af26d23d96ed32a99b7956
Maxim Byts <maxim.90svet@gmail.com>
2021-05-06 23:47:00 +0300

    refactoring (hotel-refactoring+)

--- src/components/invoice-card/invoice-card.js
+++ src/components/invoice-card/invoice-card.js
@@ -1,1 +1,51 @@
 'use strict';
+
+import { BEMComponent } from 'scripts/BEMComponent';
+import { DropdownDate } from 'components/dropdown-date/dropdown-date';
+import { DropdownGuests } from 'components/dropdown-guests/dropdown-guests';
+
+const InvoiceCard = ((document) => {
+  const ClassName = {
+    ROOT : 'js-invoice-card',
+  };
+
+  const Name = {
+    FORM : 'invoice-card',
+  }
+
+  const Selector = {
+    DROPDOWN_DATE   : '.js-dropdown-date',
+    DROPDOWN_GUESTS : '.js-dropdown-guests',
+  };
+
+  class InvoiceCard extends BEMComponent {
+    constructor(element) {
+      super(element, 'invoice-card');
+
+      this.nodes = {};
+      this.components = {};
+
+      this._connectBasis();
+      this._initComponents();
+    }
+
+    _connectBasis() {
+      this.nodes.form = document.forms[Name.FORM];
+      this.nodes.dropdownDate = this.root.querySelector(Selector.DROPDOWN_DATE);
+      this.nodes.dropdownGuests = this.root.querySelector(Selector.DROPDOWN_GUESTS);
+    }
+
+    _initComponents() {
+      this.components.dropdownDate = new DropdownDate(this.nodes.dropdownDate);
+      this.components.dropdownGuests = new DropdownGuests(this.nodes.dropdownGuests);
+    }
+  }
+
+  const initInvoiceCard = BEMComponent.makeAutoInitializer(InvoiceCard, ClassName.ROOT);
+
+  document.addEventListener('DOMContentLoaded', initInvoiceCard);
+
+  return InvoiceCard;
+})(document);
+
+export { InvoiceCard }
--- src/templates/form-elements/form-elements.js
+++ src/templates/form-elements/form-elements.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import jQuery from 'jquery';
 
 import { DropdownDate } from 'components/dropdown-date/dropdown-date';
